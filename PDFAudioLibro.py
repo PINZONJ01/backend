f0058582e9ac9120710af9338d996e7fb797cd6f
PINZONJ01 <pinzonj01s0124@gmail.com>
2024-05-03 13:35:52 -0500

    Con Comentarios

--- PDFAudioLibro.py
+++ PDFAudioLibro.py
@@ -1,15 +1,15 @@
-import pyttsx3
-import PyPDF2
-from tkinter.filedialog import *
+import pyttsx3  # Importa la biblioteca pyttsx3 para la conversión de texto a voz.
+import PyPDF2  # Importa la biblioteca PyPDF2 para manipular archivos PDF.
+from tkinter.filedialog import *  # Importa todo desde tkinter.filedialog para usar cuadros de diálogo de archivos.
 
-libro = askopenfilename()
+libro = askopenfilename()  # Abre un cuadro de diálogo para que el usuario seleccione un archivo PDF y guarda la ruta del archivo seleccionado.
 
-pdf = PyPDF2.PdfReader(libro)  # Cambiado de PdfFileReader a PdfReader
-pages = len(pdf.pages)  # Corregido de pdf.numPages a len(pdf.pages)
+pdf = PyPDF2.PdfReader(libro)  # Abre el archivo PDF seleccionado por el usuario para su lectura.
+pages = len(pdf.pages)  # Obtiene el número total de páginas en el archivo PDF.
 
-for num in range(0, pages):
-    page = pdf.pages[num]  # Actualizado para usar pdf.pages[num]
-    text = page.extract_text()  # Actualizado a extract_text() según la nueva API
-    player = pyttsx3.init()
-    player.say(text)
-    player.runAndWait()
+for num in range(0, pages):  # Itera sobre cada página del archivo PDF.
+    page = pdf.pages[num]  # Obtiene la página actual en la iteración.
+    text = page.extract_text()  # Extrae el texto de la página actual.
+    player = pyttsx3.init()  # Inicializa el motor de texto a voz.
+    player.say(text)  # Agrega el texto extraído a la cola de mensajes a ser leídos en voz alta.
+    player.runAndWait()  # Procesa la cola de mensajes, leyendo el texto en voz alta, y espera hasta que la lectura termine antes de continuar.
